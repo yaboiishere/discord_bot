4f51660d2f59436352c3a7819bc0e50610cca46f
Mihail Dobrev <yaboiishere>
2019-09-06 20:09:13 +0300

    activity watching

--- app/discord/events.rb
+++ app/discord/events.rb
@@ -6,7 +6,7 @@ module Discord
 			scheduler = Rufus::Scheduler.new
 			randold=1
 			scheduler.every '2s' do
-				gameOrWatch = rand(2)
+				gameOrWatch = rand(1)
 				if gameOrWatch
 					games = ["with your mom", "with fire", "with myself", "with knifes", "with my pickle", "a tune on the world's smallest violin", "with the dark arts", "with death", "dead"]
 					num1 = rand(games.count)
